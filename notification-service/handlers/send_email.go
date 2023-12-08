1f6032134be2f447c6d2c1d6617e9439405c54f8
guruprashanth78 <guruprashanth78@gmail.com>
2023-12-09 01:15:27 +0530

    fix: moved sender email to env

--- notification-service/handlers/send_email.go
+++ notification-service/handlers/send_email.go
@@ -8,14 +8,16 @@ import (
 	"gopkg.in/gomail.v2"
 
 	"go-notification-service/models"
+	"go-notification-service/utils"
 )
 
 func SendMail(receiver models.Order) {
 	m := gomail.NewMessage()
-	m.SetHeader("From", "guruprashanth78@gmail.com")
+	senderEmail := utils.GetEnvVar("SENDER_EMAIL")
+	m.SetHeader("From", senderEmail)
 	m.SetHeader("To", receiver.Email)
 	m.SetHeader("Subject", "From Go-Shoppy")
-	m.SetBody("text/plain", fmt.Sprintf("Ohayo ! You have ordered %s which is %f dollars", receiver.Title, receiver.Price))
+	m.SetBody("text/plain", fmt.Sprintf("Greetings from Go-Shoppy! You have ordered %s which is %f dollars", receiver.Title, receiver.Price))
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, "guruprashanth78@gmail.com", "fphlpsgkvvleadip")
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
